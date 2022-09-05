1195c7bebb0f960fb560d5a61f2ab3b6150df88c
Kledson Basso <kledsonhugo@gmail.com>
2022-09-05 12:17:20 -0300

    S3 objects with context set

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ variable "bucket_name" {
     default = "static-site-kledson-basso"
 }
 
-variable "website" {
+variable "website_endpoint" {
     default = "true"
 }
 
@@ -21,15 +21,16 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 output "aws_s3_bucket_website_endpoint" {
-    value = "${var.website =="true"? aws_s3_bucket.bucket.website_endpoint : ""}"
+    value = "${var.website_endpoint =="true"? aws_s3_bucket.bucket.website_endpoint : ""}"
 }
 
 # S3 BUCKET OBJECTS
 resource "aws_s3_bucket_object" "bucket-objects" {
-    bucket   = aws_s3_bucket.bucket.id
-    for_each = fileset("data/", "*")
-    key      = each.value
-    source   = "data/${each.value}"
-    acl      = "public-read"
-    etag     = "${md5(file("data/${each.value}"))}"
+    bucket       = aws_s3_bucket.bucket.id
+    for_each     = fileset("data/", "*")
+    key          = each.value
+    source       = "data/${each.value}"
+    acl          = "public-read"
+    content_type = "text/html"
+    etag         = "${md5(file("data/${each.value}"))}"
 }
