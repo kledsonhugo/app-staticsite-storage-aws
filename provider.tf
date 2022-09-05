8888c0e836227803d9047fd9754cd2b93e598099
Kledson Basso <kledsonhugo@gmail.com>
2022-09-05 16:05:45 -0300

    Terraform Provider Backend in S3

--- provider.tf
+++ provider.tf
@@ -1,11 +1,21 @@
 # PROVIDER
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket                  = "static-site-kledson-basso-tf-state"
+    key                     = "terraform.tfstate"
+    region                  = "us-east-1"
+    dynamodb_table          = "static-site-kledson-basso-table"
+    shared_credentials_file = ".aws/credentials"
+  }
+
 }
 
 # PROVIDER CONFIG
